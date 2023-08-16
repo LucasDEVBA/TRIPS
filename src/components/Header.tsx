78eea6ead329ee6fe0b87a2c5cf643d2c10510aa
Lucas <lucasdevba@gmail.com>
2023-08-16 17:42:50 -0300

    feat: add link to home in click log and includes z-index in menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import Image from "next/image";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { AiOutlineMenu } from "react-icons/ai";
+import Link from "next/link";
 
 const Header = () => {
   const [menuIsOpen, setMenutIsOpen] = useState(false);
@@ -20,9 +21,11 @@ const Header = () => {
 
   return (
     <div className="container mx-auto p-5 py-0 h-[93px] flex justify-between items-center">
-      <div className="relative h-[32px] w-[182px]">
-        <Image fill src="/logo.png" alt="fullstack trips" />
-      </div>
+      <Link href={"/"}>
+        <div className="relative h-[32px] w-[182px]">
+          <Image fill src="/logo.png" alt="fullstack trips" />
+        </div>
+      </Link>
 
       {status === "unauthenticated" && (
         <button
@@ -49,7 +52,7 @@ const Header = () => {
             className="rounded-full shadow-lg"
           />
           {menuIsOpen && (
-            <div className="absolute top-12 left-0 w-full h-full bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
+            <div className="z-50 absolute top-12 left-0 w-full h-full bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
               <button
                 className="text-primary text-sm font-semibold"
                 onClick={handleLogoutClick}
