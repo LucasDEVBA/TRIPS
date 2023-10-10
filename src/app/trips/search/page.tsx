5627e4890ed869478a02a77787fff6c887e12b15
Lucas <lucasdevba@gmail.com>
2023-10-10 01:48:06 -0300

    "chore: corrections of typescript in search"

--- src/app/trips/search/page.tsx
+++ src/app/trips/search/page.tsx
@@ -38,12 +38,14 @@ const Trips = () => {
   return (
     <div className="container mx-auto flex flex-col items-center p-5">
       <h1 className="text-primaryDarker font-semibold text-xl">
-        {trips?.length > 0
+        {trips?.length! > 0
           ? "Hospedagens Encontradas"
           : "Não encontramos nenhuma hospedagem"}
       </h1>
       <h2 className="text-grayPrimary font-medium mb-5">
-        {trips?.length > 0 ? "Listamos as melhores hospedagens para você!" : ""}
+        {trips?.length! > 0
+          ? "Listamos as melhores hospedagens para você!"
+          : ""}
       </h2>
       <div className="flex flex-col gap-5">
         {trips?.map((trip) => <TripItem key={trip.id} trip={trip} />)}
