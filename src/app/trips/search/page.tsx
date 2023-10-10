491c4e633fa3aa10adfaa735a138dd8c9181b286
Lucas <lucasdevba@gmail.com>
2023-10-10 10:58:41 -0300

    "style: adaptes search page to desktop"

--- src/app/trips/search/page.tsx
+++ src/app/trips/search/page.tsx
@@ -29,18 +29,18 @@ const Trips = () => {
   }, []);
 
   return (
-    <div className="container mx-auto flex flex-col items-center p-5">
-      <h1 className="text-primaryDarker font-semibold text-xl">
+    <div className="container mx-auto flex flex-col items-center lg:items-start p-5 lg:pt-10">
+      <h1 className="text-primaryDarker font-semibold text-xl lg:w-full lg:text-left lg:text-[2.5rem]">
         {trips?.length! > 0
           ? "Hospedagens Encontradas"
           : "Não encontramos nenhuma hospedagem"}
       </h1>
-      <h2 className="text-grayPrimary font-medium mb-5">
+      <h2 className="text-grayPrimary font-medium mb-5 lg:mt-6 lg:w-full lg:text-left">
         {trips?.length! > 0
           ? "Listamos as melhores hospedagens para você!"
           : ""}
       </h2>
-      <div className="flex flex-col gap-5">
+      <div className="flex flex-col gap-5 lg:grid lg:grid-cols-4 lg:gap-10 lg:mt-6 lg:pb-16">
         {trips?.map((trip) => <TripItem key={trip.id} trip={trip} />)}
       </div>
     </div>
